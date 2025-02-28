7fa89bddbba5d963f5067155500ef5f0d0ccdfbf
Mehran Mazhar <mehran.mazhar@gmail.com>
2025-02-28 19:21:44 +0330

    f

--- src/hub/graphql/query.rs
+++ src/hub/graphql/query.rs
@@ -10,7 +10,7 @@ impl Query {
     #[graphql(guard = "AuthGuard")]
     pub async fn user_ride_requests(&self, ctx: &Context<'_>) -> Option<RideRequest> {
         // Get authenticated user from context - safely unwrap because AuthGuard ensures it exists
-        let auth_user = get_auth_user(ctx).expect("User should be authenticated due to AuthGuard");
+        let _auth_user = get_auth_user(ctx).expect("User should be authenticated due to AuthGuard");
         
         Some(RideRequest {
             pickup_location: "0".to_string(),
